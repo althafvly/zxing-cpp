cccbb0d03ea11dbc255e1f1066c9338c193b591c
LuK1337 <priv.luk@gmail.com>
2024-05-30 10:52:43 +0200

    Disable publication signing

--- wrappers/android/zxingcpp/build.gradle.kts
+++ wrappers/android/zxingcpp/build.gradle.kts
@@ -118,10 +118,3 @@ publishing {
         }
     }
 }
-
-signing {
-    val signingKey: String? by project
-    val signingPassword: String? by project
-    useInMemoryPgpKeys(signingKey, signingPassword)
-    sign(publishing.publications)
-}
